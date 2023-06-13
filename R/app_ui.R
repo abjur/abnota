437cbcf103ad7eccc43b26bd7e5f384211f79ac0
rmhirota <re.hirota@gmail.com>
2023-06-13 11:57:36 -0300

    update deprecated nav()

--- R/app_ui.R
+++ R/app_ui.R
@@ -7,11 +7,11 @@ app_ui <- function() {
     shiny::navbarPage(
       theme = bslib::bs_theme(bootswatch = "minty"),
       title = "ABJ | Classificação",
-      bslib::nav("Processo", mod_processo_ui("mod_processo1")),
-      bslib::nav("Adm Judicial", mod_aj_ui("mod_aj1")),
-      bslib::nav("Credores", mod_credores_ui("mod_credores1")),
-      bslib::nav("AGC", mod_agc_ui("mod_agc1")),
-      bslib::nav("Partes", mod_partes_ui("mod_partes1"))
+      bslib::nav_panel("Processo", mod_processo_ui("mod_processo1")),
+      bslib::nav_panel("Adm Judicial", mod_aj_ui("mod_aj1")),
+      bslib::nav_panel("Credores", mod_credores_ui("mod_credores1")),
+      bslib::nav_panel("AGC", mod_agc_ui("mod_agc1")),
+      bslib::nav_panel("Partes", mod_partes_ui("mod_partes1"))
     )
   )
 }
